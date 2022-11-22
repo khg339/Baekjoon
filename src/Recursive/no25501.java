dd9795a2b821e37cfb06fec6864bbe96f274f8aa
khg339 <khg33333339@gmail.com>
2022-11-23 00:02:32 +0900

    재귀의 귀재(2차)

--- src/Recursive/no25501.java
+++ src/Recursive/no25501.java
@@ -4,20 +4,14 @@ import java.util.Scanner;
 
 public class no25501 {
 
-    static int recursion(String s, int left, int right){
-        if(left >= right) return 1;
-        else if(s.charAt(left) != s.charAt(right)) return 0;
-        else return recursion(s, left+1, right-1);
-    }
-
-    static int isPalindrome(String s){
-        return recursion(s, 0, s.length()-1);
-    }
-
-    static int recursionCount(String s, int left, int right, int count){
-        if(left >= right) return count;
-        else if(s.charAt(left) != s.charAt(right)) return count;
-        else return recursionCount(s, left+1, right-1, count+1);
+    static void recursionCount(String s, int left, int right, int count){
+        if(left >= right) {
+            System.out.println(1 + " " + count);
+        }
+        else if(s.charAt(left) != s.charAt(right)) {
+            System.out.println(0 + " " + count);
+        }
+        else recursionCount(s, left+1, right-1, count+1);
     }
 
     public static void main(String[] args){
@@ -32,7 +26,7 @@ public class no25501 {
         }
 
         for(int i=0; i<T; i++){ //출력
-            System.out.println(isPalindrome(S[i]) + " " + recursionCount(S[i], 0, S[i].length()-1, 1) );
+            recursionCount(S[i], 0, S[i].length()-1, 1);
         }
 
     }
