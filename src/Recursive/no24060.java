6262e43fd7c7a0a58de9155d08142a3509cb0dd0
khg339 <khg33333339@gmail.com>
2022-11-28 00:34:00 +0900

    알고리즘 수업 - 병합 정렬 1(2차, 성공)

--- src/Recursive/no24060.java
+++ src/Recursive/no24060.java
@@ -3,15 +3,14 @@ package Recursive;
 import java.util.Scanner;
 
 public class no24060 {
-
-    static Scanner sc = new Scanner(System.in);
-
-    static int N = sc.nextInt(); //A의 크기
-    static int K = sc.nextInt(); //저장 횟수
+    static int N; //A의 크기
+    static int K;//저장 횟수
+    static int[] A, tmp;
     static int count = 0;
     static int result = -1;
 
     static void merge_sort(int[] A, int p, int r){
+        if (count > K) return ; //count가 K보다 크면 재귀 탈출
         if(p<r){
             int mid = (p+r)/2;
             merge_sort(A, p, mid); //전반부 정렬
@@ -24,47 +23,43 @@ public class no24060 {
         int i = p;
         int j = mid+1;
         int t = 0;
-        int[] tmp = new int[A.length];
 
         while(i<= mid && j<=r){
             if(A[i]<=A[j]) tmp[t++] = A[i++];
             else tmp[t++] = A[j++];
-            count++;
-
-            if(count == K){
-                result = tmp[t-1];
-            }
-
         }
 
         while(i<=mid){ //배열 왼쪽 부분이 남은 경우
             tmp[t++] = A[i++];
-            count++;
-            if(count == K){
-                result = tmp[t-1];
-            }
         }
 
         while(j<=r){ //배열 오른쪽 부분이 남은 경우
             tmp[t++] = A[j++];
-            count++;
-
-            if(count == K){
-                result = tmp[t-1];
-            }
         }
-
         i = p; t = 0;
 
         while(i<=r){ //결과를 A[]에 저장
+            count++;
+            if(count == K) { //count가 K와 같으면 result에 저장 후 탈출
+                result = tmp[t];
+                break;
+            }
             A[i++] = tmp[t++];
         }
     }
     public static void main(String[] args){
-        int[] A = new int[N];
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        K = sc.nextInt();
+
+        A = new int[N];
+        tmp = new int[N];
+
         for(int i=0; i<N; i++){ //입력
             A[i] = sc.nextInt();
         }
+
+        sc.close();
 
         merge_sort(A, 0, N-1); //병합정렬
 
