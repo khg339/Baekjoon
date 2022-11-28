a9b262b4aa13819ede945cb4301860d9c66e02d9
khg339 <khg33333339@gmail.com>
2022-11-28 21:53:29 +0900

    별 찍기 - 10(수정)

--- src/Recursive/no2447.java
+++ src/Recursive/no2447.java
@@ -1,24 +1,46 @@
 package Recursive;
 
-import java.util.Scanner;
+import java.io.*;
 
 public class no2447 {
 
-    static void star(int n){
-        if(n==1) System.out.print("*");
+    static void star(char[][] arr, int startX, int startY, int N){
+        if(N==1){  //기본 별찍기
+            arr[startX][startY] = '*';
+            return; //재귀 탈출
+        }
 
         for(int i=0; i<3; i++){
             for(int j=0; j<3; j++){
-                star(n/3);
+                if(!(i==1 && j==1)){ //i, j가 모두 1인 경우(가운데) 빼고
+                    star(arr, startX + i*(N/3), startY + j*(N/3), N/3);
+                }
             }
-            System.out.println();
         }
     }
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int N = sc.nextInt(); //3의 거듭 제곱
 
-        star(N);
+        int N = Integer.parseInt(br.readLine()); //3의 거듭 제곱
+        char[][] arr = new char[N][N];
+
+        for(int i=0; i<N; i++){ //배열 초기화
+            for(int j=0; j<N; j++){
+                arr[i][j] = ' ';
+            }
+        }
+
+        star(arr, 0, 0, N); //별찍기
+
+        // 출력
+        for(int i=0; i<N; i++) {
+            bw.write(arr[i]);
+            bw.write('\n');
+        }
+
+        bw.flush();
+        bw.close();
     }
 }
