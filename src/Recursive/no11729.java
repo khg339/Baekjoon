36817d7ea74aa536e25c986e6cd9bae7b4278169
khg339 <khg33333339@gmail.com>
2022-11-29 18:11:02 +0900

    하노이 탑 이동 순서

--- src/Recursive/no11729.java
+++ src/Recursive/no11729.java
@@ -1,21 +1,36 @@
 package Recursive;
 
-import java.util.Scanner;
+import java.io.*;
 import java.lang.Math;
 
 public class no11729 {
 
-    static int hanoi(int n){
-        if(n==1) return 1;
-        else return hanoi(n-1) + (int)Math.pow(2,n-1);
+    static int N;
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    public static void main(String[] args) throws IOException {
+        N = Integer.parseInt(br.readLine());
+
+        bw.write((int) Math.pow(2, N) - 1 + "\n"); //하노이탑 이동 횟수
+
+        hanoi(N, 1, 2, 3); //이동 출력
+
+        bw.flush();
+        bw.close();
     }
 
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
+    static void hanoi(int n, int start, int mid, int to) throws IOException {
+        if (n == 1) { //1개라면
+            bw.write(start + " " + to + "\n");
+            return; //재귀 멈춤
+        }
 
-        int N = sc.nextInt();
+        hanoi(n - 1, start, to, mid); //n-1개를 목적지가 아닌 곳에 옮김
 
-        System.out.println(hanoi(N));
+        bw.write(start + " " + to + "\n"); //n번째 원판을 목적지로 옮김
 
+        hanoi(n - 1, mid, start, to); //n-1개를 다시 목적지로 옮김
     }
 }
